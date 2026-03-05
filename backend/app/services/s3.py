112360c48c5ffff0b05ab2c9eccd1d9ffe29298e
Lyonsaldanha <saldanhalyon@yahoo.in>
2026-03-06 02:00:05 +0530

    bug fixes

--- backend/app/services/s3.py
+++ backend/app/services/s3.py
@@ -20,8 +20,6 @@ class S3Service:
         try:
             self.s3_client = boto3.client(
                 's3',
-                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
                 region_name=settings.AWS_REGION
             )
             self.audio_bucket = settings.AWS_S3_BUCKET_AUDIO
